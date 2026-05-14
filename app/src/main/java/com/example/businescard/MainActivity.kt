11dfd0c1f0d011836833f992fc7ec4ca8cc80b08
Thomaspara <thomaspara96@gmail.com>
2026-05-14 05:34:32 +0300

    .

--- app/src/main/java/com/example/businescard/MainActivity.kt
+++ app/src/main/java/com/example/businescard/MainActivity.kt
@@ -4,13 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.businescard.ui.theme.BusinesCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,7 +30,6 @@ class MainActivity : ComponentActivity() {
             BusinesCardTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,12 +39,54 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun Greeting(modifier: Modifier = Modifier) {
+    Box(
         modifier = modifier
-    )
+            .fillMaxSize()
+            .background(Color(0xFFD2E8D4))
+    ) {
+        Column {
+            Row {
+                WidgetOne()
+            }
+            Row {
+                WidgetTwo()
+            }
+        }
+    }
 }
+
+@Composable
+fun WidgetOne() {
+    Column {
+        Row {
+            Image(
+                painter = painterResource(id = R.drawable.android_logo),
+                contentDescription = null
+            )
+        }
+        Row {
+            Text(
+                text = "Thomas Paravaitsis",
+                color = Color.Black,
+                fontSize = 30.sp
+            )
+        }
+        Row {
+            Text(
+                text = "Android Developer Extraordinary",
+                color = Color(0xFF006400),
+                fontWeight = FontWeight.Bold
+            )
+        }
+    }
+}
+
+@Composable
+fun WidgetTwo() {
+
+}
+
 
 @Preview(
     showBackground = true,
@@ -45,6 +95,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     BusinesCardTheme {
-        Greeting("Android")
+        Greeting()
     }
 }
